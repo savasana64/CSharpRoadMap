23f093b4d8af258bbbefdc030b61ac212356209f
savasana64 <128321910+savasana64@users.noreply.github.com>
2023-08-11 23:28:19 +0200

    String concatenation

---
+++ Concat.cs
@@ -0,0 +1,31 @@
+﻿int age = 37;
+string name = "Mahta";
+
+//concatination is what we have seen with +
+
+//string formatting
+
+
+Console.WriteLine("String formatting");
+Console.WriteLine("Hello my name is {0} , I am {1} years old", name, age);
+
+//String interpolation
+
+Console.WriteLine("String interpolation");
+Console.WriteLine($"Hello my name is {name} , I am {age} years old");
+
+//Verbatim strings
+
+Console.WriteLine("String verbatim");
+//Go and see the tutorial
+
+//if you use @ they are not escaped
+
+Console.WriteLine(@"D:\MyProjects\Udemy\CPP_Management");
+
+Console.WriteLine(@"Muhaha \n you have no power here");
+
+
+
+
+
