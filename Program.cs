1b32323c5576e953e53495aca479c388b06f9c4d
savasana64 <128321910+savasana64@users.noreply.github.com>
2023-08-11 22:03:37 +0200

    Hello worlding with csharp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//This code type hello world on the screen using writeline from the Console class in sharp
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
