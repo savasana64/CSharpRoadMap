5bc77910baa9a2dc71c041d4bacb935f9621106e
savasana64 <128321910+savasana64@users.noreply.github.com>
2023-08-11 23:46:48 +0200

    Ternary or enhanced if

---
+++ Ternary.cs
@@ -0,0 +1,17 @@
+﻿int temperature = -5;
+string stateOfMatter;
+
+stateOfMatter = temperature < 0 ? "solid" : "liquid";
+
+Console.WriteLine(stateOfMatter);
+
+temperature += 30;
+
+//here a nested if in ternary
+
+stateOfMatter = temperature > 100 ? "gas" : temperature < 0 ? "solid" : "liquid";
+
+//note: the expression a ? b : c ? d: e is evaluated as:
+//a ? b : (c ? d: e)
+
+Console.WriteLine(stateOfMatter);
