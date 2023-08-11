ee50cc5a6aec26f3e75f51ab8b0699ed6ad8a08a
savasana64 <128321910+savasana64@users.noreply.github.com>
2023-08-11 23:23:44 +0200

    Parsing string to numbers

---
+++ Parsing.cs
@@ -0,0 +1,28 @@
+﻿//parse numbers out of strings
+
+string myString = "15";
+string mySecondString = "13";
+
+//parse strings to integers
+
+int num1 = Int32.Parse(myString);
+int num2 = Int32.Parse(mySecondString);
+
+int resultInt = num1 + num2;
+Console.WriteLine(resultInt);
+
+string stringForFloat = "0.85"; // datatype should be float
+string stringForInt = "12345"; // datatype should be int
+
+//parse strings to float
+
+float parsedFloat = float.Parse(stringForFloat);
+int parsedInt = Int32.Parse(stringForInt);
+
+Console.WriteLine(parsedInt);
+Console.WriteLine(parsedFloat);
+
+
+
+
+
