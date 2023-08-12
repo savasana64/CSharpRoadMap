0fd234b76f2960e4c960a8bbaad763c1befa3826
savasana64 <128321910+savasana64@users.noreply.github.com>
2023-08-12 15:53:07 +0200

    Interface example

---
+++ InterfacExample.cs
@@ -0,0 +1,13 @@
+﻿namespace InterfaceExample
+{
+    internal class InterfacExample
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+
+            Console.WriteLine(t2.Equals(t1));
+        }
+    }
+}
