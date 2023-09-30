fdcc00f4a296cc42d67b7258b377c8b5c975f55e
savasana64 <128321910+savasana64@users.noreply.github.com>
2023-09-30 16:28:44 +0200

    Static class example

---
+++ DataBase.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticExample
+{
+    //this class is static therefore all its members are static
+    //To call a method from this class only the class name is enough: you do not need to instantiate an object
+    public static class DataBase
+    {
+        private static List<int> dbNumbers = new List<int>();
+
+        public static void AddIndexToDB(int index)
+        {
+            dbNumbers.Add(index);
+        }
+    }
+}
